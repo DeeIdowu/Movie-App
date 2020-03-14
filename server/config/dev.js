5b97c49a526103908b54cd7f37c2d9d394b9640f
Dennis Idowu <dennis_idowu@live.com>
2020-03-14 02:18:29 -0400

    implemented connection for mongodb database to application

---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://sublimedee:micheal23@movieappdatabase-wfsmy.mongodb.net/test?retryWrites=true&w=majority" //implementation of key from mongodb
+};
